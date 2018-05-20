94522ceeaf6b468184042753e46d09e91cef6bab
john.lonergan@gmail.com <Qqqqqqqq1>
2018-05-20 23:48:19 +0100

    added data service for the rest of the exploration

--- datasource/src/main/java/DataService.java
+++ datasource/src/main/java/DataService.java
@@ -4,20 +4,33 @@ import java.io.StringWriter;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 class Roots {
-    String person() {return "/person";}
-    String location() {return "/location";}
 
-    String person(String id) { return person() + "/" + id; }
-    String location(String id) { return location() + "/" + id; }
+    private static String ROOT_PLACEHOLDER = "ROOT";
+
+    static String localise(String s, String thisRoot) {
+        return s.replaceAll(ROOT_PLACEHOLDER, thisRoot);
+    }
+
+    static String searchPersonPath() {return "/search";}
+    static String personPath() {return "/person";}
+    static String locationPath() {return "/location";}
+
+    static String personUrl() {return ROOT_PLACEHOLDER + personPath();}
+    static String locationUrl() {return ROOT_PLACEHOLDER + locationPath();}
+
+    static String personUrl(String id) { return id == null? null : personUrl() + "/" + id; }
+    static String locationUrl(String id) { return id == null? null : locationUrl() + "/" + id; }
 };
-
+                                                                                                                                                                                                                                                                        @JsonInclude(JsonInclude.Include.NON_NULL)
 class Person {
     public String id;
     public String selfUrl;
@@ -26,36 +39,34 @@ class Person {
     public String locationUrl;
 
     public static Person make(
-            Roots roots,
             String _id,
             String _name,
             String _location
     ) {
         return new Person() {{
             this.id = _id;
-            this.selfUrl = roots.person(_id);
+            this.selfUrl = Roots.personUrl(_id);
             this.name = _name;
             this.location = _location;
-            this.locationUrl = roots.location(_location);
+            this.locationUrl = Roots.locationUrl(_location);
         }};
     }
 }
-
+@JsonInclude(JsonInclude.Include.NON_NULL)
 class Location {
-    public String location;
+    public String id;
     public String selfUrl;
     public String city;
     public String country;
 
     public static Location make(
-            Roots roots,
             String _id,
             String _city,
             String _country
     ) {
         return new Location() {{
-            this.location = _id;
-            this.selfUrl = roots.location(_id);
+            this.id = _id;
+            this.selfUrl = Roots.locationUrl(_id);
             this.city = _city;
             this.country = _country;
         }};
@@ -63,14 +74,13 @@ class Location {
 }
 
 public class DataService {
-    static String PERSON_ROOT = "PERSON_ROOT";
-    static String LOCATION_ROOT = "PERSON_ROOT";
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
-        server.createContext("/person", new PersonHandler());
-        server.createContext("/location", new LocationHandler());
+        server.createContext(Roots.searchPersonPath(), new SearchPersonHandler());
+        server.createContext(Roots.personPath(), new PersonHandler());
+        server.createContext(Roots.locationPath(), new LocationHandler());
         server.setExecutor(null); // creates a default executor
 
         int p = 10;
@@ -80,6 +90,7 @@ public class DataService {
         people = initPerson(roots, p, l);
         locations = initLocation(roots, l);
 
+        System.out.println("Port 8000");
         server.start();
 
     }
@@ -93,10 +104,19 @@ public class DataService {
                 "FR"
         };
 
+
+    static String[] names= {
+            "John",
+            "Katie",
+            "Gracie"
+    };
+
     private static Person[] initPerson(Roots root, int p, int l) {
         Person[] people = new Person[p];
         for (int id = 0; id < p; id++) {
-            people[id] = Person.make(root, "" + id, "person" + id, "" + (id % l));
+
+            String location = id % l == 0? null : "" + id % l; // set some locations to null/unknown
+            people[id] = Person.make("" + id, names[id%names.length] + id, location);
         }
         return people;
     }
@@ -104,23 +124,57 @@ public class DataService {
     private static Location[] initLocation(Roots roots, int l) {
         Location[] locations = new Location[l];
         for (int id = 0; id < l; id++) {
-            locations[id] = Location.make(roots,"" + id, "city" + id, countries[id % countries.length]);
+            locations[id] = Location.make("" + id, "city" + id, countries[id % countries.length]);
         }
         return locations;
     }
 
     static class PersonHandler implements HttpHandler {
+
         ObjectMapper mapper = new ObjectMapper();
 
         @Override
         public void handle(HttpExchange t) throws IOException {
 
             t.getResponseHeaders().add("content-type", "application/json");
-
             OutputStream os = t.getResponseBody();
             try {
 
-                Object responseObject = getResponseObject(t, people, "/person");
+                Object responseObject = getResponseObject(t, people, Roots.personPath());
+
+                StringWriter response = new StringWriter(100);
+                mapper.writeValue(response, responseObject);
+
+                String b = localise(t, response);
+
+                t.sendResponseHeaders(200, b.length());
+                os.write(b.toString().getBytes("UTF-8"));
+
+            } catch(Exception ex) {
+                System.out.println("EX " + ex);
+                throw ex;
+            } finally {
+
+                os.close();
+            }
+        }
+    }
+
+    static class SearchPersonHandler implements HttpHandler {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+
+            t.getResponseHeaders().add("content-type", "application/json");
+            OutputStream os = t.getResponseBody();
+
+            try {
+
+                Map<String, String> params = getQueryMap(t.getRequestURI().getQuery());
+                String nameSearch = params.get("name");
+                Person[] responseObject = Stream.of(DataService.people).filter(x-> x.name.contains(nameSearch)).toArray(Person[]::new);
 
                 StringWriter response = new StringWriter(100);
                 mapper.writeValue(response, responseObject);
@@ -150,7 +204,7 @@ public class DataService {
 
             OutputStream os = t.getResponseBody();
             try {
-                Object responseValue = getResponseObject(t, locations, "/location");
+                Object responseValue = getResponseObject(t, locations, Roots.locationPath());
 
                 StringWriter response = new StringWriter(100);
                 mapper.writeValue(response, responseValue);
@@ -191,7 +245,7 @@ public class DataService {
 
     private static String localise(HttpExchange t, StringWriter response) {
         String thisRoot = "http://" + t.getRequestHeaders().getFirst("host");
-        return response.getBuffer().toString().replaceAll(root_placeholder, thisRoot);
+        return Roots.localise(response.getBuffer().toString(), thisRoot);
     }
 
     public static Map<String, String> getQueryMap(String query)
