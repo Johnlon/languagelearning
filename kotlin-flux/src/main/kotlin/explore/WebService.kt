a06c4a3e3752c42ea0bf842a0241109b4528e4c3
john.lonergan@gmail.com <Qqqqqqqq1>
2018-05-30 02:55:25 +0100

    kotlin boot and flux apps now structured same way

--- kotlin-flux/src/main/kotlin/explore/WebService.kt
+++ kotlin-flux/src/main/kotlin/explore/WebService.kt
@@ -3,14 +3,43 @@ package explore
 import org.springframework.core.io.ClassPathResource
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.http.MediaType.TEXT_HTML
+import org.springframework.web.reactive.function.server.RouterFunctions
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
+import org.springframework.web.server.WebHandler
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toFlux
 
 
-class PersonHandler(val renderHtml: Boolean) {
+fun beans() = org.springframework.context.support.beans {
+    bean<Routes>()
+    bean<WebHandler>("webHandler") {
+        RouterFunctions.toWebHandler(
+                ref<Routes>().router()
+        )
+    }
+}
+
+
+class Routes {
+
+    val personHandlerHtml =  SearchHandler(renderHtml = true)
+    val personHandlerJson =  SearchHandler(renderHtml = false)
+
+    fun router() = router {
+        accept(TEXT_HTML).nest {
+            GET("/search", personHandlerHtml::search)
+        }
+        accept(APPLICATION_JSON).nest {
+            GET("/search", personHandlerJson::search)
+        }
+
+        resources("/**", ClassPathResource("static/"))
+    }
+}
+
+class SearchHandler(val renderHtml: Boolean) {
     val svc = Service()
     val renderer = Renderer()
 
@@ -35,20 +64,3 @@ class PersonHandler(val renderHtml: Boolean) {
     }
 }
 
-
-class Routes {
-
-    val personHandlerHtml =  PersonHandler(renderHtml = true)
-    val personHandlerJson =  PersonHandler(renderHtml = false)
-
-    fun router() = router {
-        accept(TEXT_HTML).nest {
-            GET("/search", personHandlerHtml::search)
-        }
-        accept(APPLICATION_JSON).nest {
-            GET("/search", personHandlerJson::search)
-        }
-
-        resources("/**", ClassPathResource("static/"))
-    }
-}
